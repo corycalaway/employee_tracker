8e1d80ea261349f74c31fe65a5e6ce22ee5a6814
Cory Calaway <cory.c.calaway@gmail.com>
2021-02-07 13:37:25 -0700

    added title to start

---
+++ routes/dbRoutes/Employees.js
@@ -0,0 +1,38 @@
+const fs = require("fs");
+const inquirer = require("inquirer");
+
+class Database {
+    constructor(){}
+
+    // function to add a department db
+    addDepartment() {
+
+    }
+
+    // function to add a role to db
+    addRole() {
+
+    }
+
+    // function to add employee to db
+    addEmployee() {
+
+    }
+
+    // function to update employee in db
+    updateEmployee() {
+
+    }
+
+    // view all departments function
+    viewDepartments() {
+
+    }
+
+    // view all roles function
+    viewEmployees() {
+
+    }
+}
+
+module.exports = Database;
--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const mysql = require('mysql2');
 const cTable = require('console.table');
 const inquirer = require('inquirer');
-
+const Database = require('./routes/dbRoutes/Employees')
+const logo = require('asciiart-logo');
+// const config = require('./package.json');
 // Creates the connection to database
 const connection = mysql.createConnection({
   host: 'localhost',
@@ -13,17 +15,65 @@ const connection = mysql.createConnection({
   database: 'employee_DB'
 });
 
+
+
+// afterConnection = () => {
+//   connection.query('SELECT * FROM employee', function(err, res) {
+//     if (err) throw err;
+//     console.log(res);
+//     console.table(res)
+//     connection.end();
+//   });
+// };
+
+// view different sections
+
+class Aplication {
+  constructor() {
+
+  }
+
+//   startApp() {
+//     console.log(logo({name: 'Employee Tracker',
+//   font: 'Speed',
+//   lineChars: 10,
+//   padding: 2,
+//   margin: 3,
+//   borderColor: 'grey',
+//   logoColor: 'bold-green',
+//   textColor: 'green',
+// }).emptyLine()
+// .right()
+// .emptyLine()
+// .center()
+// .render()
+// );
+
+//     }
+
+startApp(){
+  console.log(logo({name: 'Employee Tracker',
+font: 'Speed'}).render());
+connection.end();
+}
+
+}
+// afterConnection = () => {
+//   connection.query(`SELECT * FROM employee`, function(err, res) {
+//     if (err) throw err;
+//     console.log(res);
+//     console.table(res)
+//     connection.end();
+//   });
+// };
+
+// `SELECT id, first_name, last_name 
+//   FROM employee
+//     LEFT JOIN roles ON employee.role_id = roles.id`
+
 connection.connect(err => {
   if (err) throw err;
   console.log('connected as id ' + connection.threadId);
-  afterConnection();
-});
+  new Aplication().startApp();
 
-afterConnection = () => {
-  connection.query('SELECT * FROM employee', function(err, res) {
-    if (err) throw err;
-    console.log(res);
-    console.table(res)
-    connection.end();
-  });
-};
+});
